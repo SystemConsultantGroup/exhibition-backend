3f0ea962038572335725bbdd5fb10e25af40ee6d
Stringju <iam@2tle.io>
2026-03-02 18:11:26 +0900

    fix: cors 수정

--- src/main/java/kr/ac/skku/scg/exhibition/global/auth/security/JwtAuthenticationFilter.java
+++ src/main/java/kr/ac/skku/scg/exhibition/global/auth/security/JwtAuthenticationFilter.java
@@ -27,6 +27,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
 
         if (authorization != null && !authorization.isBlank()) {
