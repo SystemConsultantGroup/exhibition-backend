5a8ff975efd86f19ff2cf5207ce32d8f4ebe192a
Hyunjun Yang <iam@2tle.io>
2026-03-27 23:24:07 +0900

    fix: remove header parser from media controller

--- src/main/java/kr/ac/skku/scg/exhibition/media/repository/MediaAssetRepository.java
+++ src/main/java/kr/ac/skku/scg/exhibition/media/repository/MediaAssetRepository.java
@@ -5,6 +5,4 @@ import kr.ac.skku.scg.exhibition.media.domain.MediaAssetEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MediaAssetRepository extends JpaRepository<MediaAssetEntity, UUID> {
-
-    java.util.Optional<MediaAssetEntity> findByIdAndExhibition_Id(UUID id, UUID exhibitionId);
 }
